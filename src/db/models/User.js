a2ef5dc645954b5d2f995a095df5a8037fed48f8
karang <karan.galiya@cybercomcreation.com>
2025-11-11 18:43:35 +0530

    feat: created the user table

---
+++ src/db/models/User.js
@@ -0,0 +1,113 @@
+"use strict";
+
+import { Model } from "sequelize";
+
+export default (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index.js` file will call this method automatically.
+     */
+    static associate(models) {
+      // Self-referential association for referrals (optional)
+      User.belongsTo(models.User, {
+        foreignKey: "referred_by",
+        as: "referrer",
+      });
+
+      User.hasMany(models.User, {
+        foreignKey: "referred_by",
+        as: "referrals",
+      });
+    }
+  }
+
+  User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      user_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      first_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      full_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      google_id: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      profile_picture: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      auth_provider: {
+        type: DataTypes.ENUM("local", "google"),
+        defaultValue: "local",
+        allowNull: false,
+      },
+      referral_code: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      referral_points: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      referred_by: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      status: {
+        type: DataTypes.ENUM("active", "inactive", "banned"),
+        defaultValue: "active",
+        allowNull: false,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      deleted_at: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+    },
+    {
+      sequelize,
+      modelName: "User",
+      tableName: "users",
+      timestamps: true,
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+      deletedAt: "deleted_at",
+      paranoid: true,
+      underscored: true,
+    }
+  );
+
+  return User;
+};
